c82345f5d84f67a2f8a173e1273772ced2f691ec
신관준 <shinkansan@dgist.ac.kr>
2021-08-05 14:54:52 +0900

    Add files via upload

--- RT_client.py
+++ RT_client.py
@@ -36,14 +36,20 @@ class realSense:
 
     def openPipeline(self):
         print("Open RS Pipeline")
+        pipeline = rs.pipeline()
         cfg = rs.config()
+        pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+        pipeline_profile = cfg.resolve(pipeline_wrapper)
+        device = pipeline_profile.get_device()
+
         cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        device_product_line = str(device.get_info(rs.camera_info.product_line))
 
         if device_product_line == 'L500':
-            config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+            cfg.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
         else:
-            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        pipeline = rs.pipeline()
+            cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        
         pipeline_profile = pipeline.start(cfg)
         sensor = pipeline_profile.get_device().first_depth_sensor()
         depth_scale = sensor.get_depth_scale()
